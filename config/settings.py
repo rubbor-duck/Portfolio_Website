aa118ce120dc31852ab3e9e4ccc99ea8dadebcdb
rubbor-duck <nelson.jordan10@hotmail.com>
2026-09-24 17:17:20 -0600

    Getting 2nd web page setup

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'portfolio.apps.PortfolioConfig',
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
